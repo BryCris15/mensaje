aa6d27f8a378e74fb1ab9be2ef2ebe1844bed77a
BryCris15 <bcmalla@uce.edu.ec>
2025-05-14 15:16:21 -0500

    Cambio de mensaje

--- hola.py
+++ hola.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hola_mundo():
-    return '¡Hola, mundo!'
+    return '¡Ya pase distribuida!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
